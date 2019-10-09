3af18ffc9e3b3e11d7b4e3413d91675691dc204a
BhumikaManocha <bhumika.manocha@gmail.com>
2019-10-10 01:15:05 +0530

    register backend added

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
-
 const app = express();
+const bcrypt = require('bcrypt');
+
+const users = [];
 
 app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({extended:false}));
 
 // app.use(express.static('play/public'));
 // app.use(express.static(path.resolve('./public')));
@@ -16,6 +20,41 @@ app.use('*/images',express.static('public/images'));
 app.get('/',(req,res)=>{
     res.render('index')
 })
+
+app.get('/register',(req,res) => {
+    res.render('register')
+})
+
+app.post('/register', async(req,res) => {
+    try{
+        const hashedpassword = await bcrypt.hash(req.body.password,10);
+        users.push({
+            name : req.body.name,
+            gamingname :req.body.gamingname,
+            dateofbirth : req.body.dateofbirth,
+            email : req.body.email,
+            password: hashedpassword
+        })
+        res.redirect('/login');
+    }
+    catch{
+        res.redirect('/regsiter');
+    }
+    console.log("done")
+    console.log(users);
+
+})
+
+app.get('/login',(req,res) => {
+    res.render('login')
+})
+
+app.post('/login',(req,res) => {
+
+
+})
+
+
 
 app.get('/dice',(req,res)=>{
     res.render('dice')
